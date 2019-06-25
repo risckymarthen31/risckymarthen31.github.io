2cace3bdd0c2f8e8203c4b20c8bf0215000d26be
risckymarthen31 <37300935+risckymarthen31@users.noreply.github.com>
2019-06-25 15:00:42 +0700

    v9

---
+++ js/portofolio.js
@@ -0,0 +1,24 @@
+const _count = 1;
+const _url = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=1820664604.1731dfd.fb778f67629f404c827d8efa41347aec&count=' + _count;
+fetch(_url)
+    .then((resp) => resp.json())
+    .then(function (data) {
+        for (var i = 0; i < _count; i++) {
+            const _tags = "tags" + i; //untuk key tags
+            const _iSrc = "src" + i; //untuk key sources images
+
+            localStorage.setItem(_tags, data.data[i].tags[0]);
+            localStorage.setItem(_iSrc, data.data[i].images.standard_resolution.url);
+        }
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+
+var _html = "";
+for (var i = 0; i < _count; i++) {
+    const _tags = "tags" + i; //untuk key tags
+    const _iSrc = "src" + i; //untuk key sources images
+    _html += "<div class='col-lg-4 col-md-6 portfolio-item filter-"+localStorage.getItem(_tags)+"'><div class='portfolio-wrap'><img src='" + localStorage.getItem(_iSrc) + "' class='img-fluid' alt=''><div class='portfolio-info'><h4><a href='#'>" + localStorage.getItem(_tags) + "</a></h4><p>" + localStorage.getItem(_tags) + "</p><div><a href='" + localStorage.getItem(_iSrc) + "' data-lightbox='portfolio' data-title='" + localStorage.getItem(_tags) + "' class='link-preview' title='Preview'><i class='ion ion-eye'></i></a><a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a></div></div></div></div>";
+}
+document.getElementById("portofolio_photos").innerHTML = _html;
--- service-worker.js
+++ service-worker.js
@@ -1,69 +1,39 @@
 const FILES_TO_CACHE = [ 'offline.html'];
 const CACHE_NAME="namaCache";
 self.addEventListener('activate', (evt) => {
-
-  evt.waitUntil( 
+evt.waitUntil( 
     caches.keys().then((keyList) => { 
-      return Promise.all(keyList.map((key) => { 
-        if (key !== CACHE_NAME) { 
-          console.log('[ServiceWorker] Removing old cache', key); 
-          return caches.delete(key); 
-        } 
-      }));
+        return Promise.all(keyList.map((key) => { 
+            if (key !== CACHE_NAME) { 
+                console.log('[ServiceWorker] Removing old cache', key); 
+                return caches.delete(key); 
+            } 
+        }));
     })
-    );
+);
 
 });
 
 self.addEventListener('fetch', (evt) => {
 
-  evt.respondWith( 
+evt.respondWith( 
     fetch(evt.request) 
-    .catch(() => { 
-      return caches.open(CACHE_NAME) 
-      .then((cache) => { 
-        return cache.match('offline.html'); 
-      }); 
-    }) 
-    ); 
-
-
-  evt.waitUntil( 
-    caches.keys().then((keyList) => { 
-      return Promise.all(keyList.map((key) => { 
-        if (key !== CACHE_NAME) { 
-          console.log('[ServiceWorker] Removing old cache', key); 
-          return caches.delete(key); 
-        } 
-      }));
-    })
-    );
-});
-
-self.addEventListener('fetch', (evt) => {
-  evt.respondWith( 
-    fetch(evt.request) 
-    .catch(() => { 
-      return caches.open(CACHE_NAME) 
-      .then((cache) => { 
-        return cache.match('offline.html'); 
-      }); 
-    }) 
-    ); 
+        .catch(() => { 
+            return caches.open(CACHE_NAME) 
+                .then((cache) => { 
+                    return cache.match('offline.html'); 
+                }); 
+        }) 
+); 
 
 });
 
 self.addEventListener('install', (evt) => {
- evt.waitUntil( 
-   caches.open(CACHE_NAME).then((cache) => { 
-
-     console.log('[ServiceWorker] Pre-caching offline page'); 
-     return cache.addAll(FILES_TO_CACHE);
-
-     console.log('[ServiceWorker] Pre-caching offline page'); 
-     return cache.addAll(FILES_TO_CACHE);
-
-   }) 
-   );
+   evt.waitUntil( 
+     caches.open(CACHE_NAME).then((cache) => { 
+         console.log('[ServiceWorker] Pre-caching offline page'); 
+         return cache.addAll(FILES_TO_CACHE);
+     }) 
+  );
  self.skipWaiting();
 });
