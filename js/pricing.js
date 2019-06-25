0687c1b75ff857b88ba6542802d48c3c559d49e6
risckymarthen31 <37300935+risckymarthen31@users.noreply.github.com>
2019-06-26 00:28:46 +0700

    vfinal

---
+++ js/pricing.js
@@ -0,0 +1,18 @@
+function plan_1() {
+    document.getElementById("number1").value = 1;
+    document.getElementById("plan1").style = "transform: scale(1.1);box-shadow: 0 20px 35px 0 rgba(0, 0, 0, 0.08);background-color: #F0F0F0;transition: 0.5s;";
+    document.getElementById("plan2").style = "transform: scale(1.0);box-shadow: 0 0px 0px 0 rgba(0, 0, 0, 0);background-color: #FFFFFF;transition: 0.5s;";
+    document.getElementById("plan3").style = "transform: scale(1.0);box-shadow: 0 0px 0px 0 rgba(0, 0, 0, 0);background-color: #FFFFFF;transition: 0.5s;";
+}
+function plan_2() {
+    document.getElementById("number1").value = 2;
+    document.getElementById("plan1").style = "transform: scale(1.0);box-shadow: 0 0px 0px 0 rgba(0, 0, 0, 0);background-color: #FFFFFF;transition: 0.5s;";
+    document.getElementById("plan2").style = "transform: scale(1.1);box-shadow: 0 20px 35px 0 rgba(0, 0, 0, 0.08);background-color: #F0F0F0;transition: 0.5s;";
+    document.getElementById("plan3").style = "transform: scale(1.0);box-shadow: 0 0px 0px 0 rgba(0, 0, 0, 0);background-color: #FFFFFF;transition: 0.5s;";
+}
+function plan_3() {
+    document.getElementById("number1").value = 3;
+    document.getElementById("plan1").style = "transform: scale(1.0);box-shadow: 0 0px 0px 0 rgba(0, 0, 0, 0);background-color: #FFFFFF;transition: 0.5s;";
+    document.getElementById("plan2").style = "transform: scale(1.0);box-shadow: 0 0px 0px 0 rgba(0, 0, 0, 0);background-color: #FFFFFF;transition: 0.5s;";
+    document.getElementById("plan3").style = "transform: scale(1.1);box-shadow: 0 20px 35px 0 rgba(0, 0, 0, 0.08);background-color: #F0F0F0;transition: 0.5s;";
+}
--- worker.js
+++ worker.js
@@ -2,13 +2,13 @@ onmessage = function(e) {
   console.log('Worker: Message received from main script');
   let result = 0;
   if (e.data[0] == 1) {
-  	result = 750000 * e.data[1];
+  	result = 250000 * e.data[1];
   }
   else if (e.data[0] == 2) {
   	result = 500000 * e.data[1];
   }
   else if (e.data[0] == 3) {
-  	result = 1200000 * e.data[1];
+  	result = 750000 * e.data[1];
   }
   else{
   	result = 0 * e.data[1];
